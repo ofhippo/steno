331b428456ee371bbb5d1e324b29a43ad44b1bb8
Dan Rufener <drufener@newrelic.com>
2018-12-28 19:27:36 -0800

    Ignore huge files but comment where to get them

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static NgramLanguageModel loadLanguageModel() {
         System.out.println("Loading Language Model (~1 minute)");
         long startTime = System.currentTimeMillis();
+        //  Google Books binaries for English from http://tomato.banatao.berkeley.edu:8080/berkeleylm_binaries/
         final StupidBackoffLm<String> lm = readGoogleLmBinary("/Users/drufener/steno/src/main/resources/eng.blm", "/Users/drufener/steno/src/main/resources/vocab_cs");
         long endTime = System.currentTimeMillis();
         System.out.println("Loaded Language Model Time (ms): " + (endTime - startTime));
