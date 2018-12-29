e8ad9996ec44dd03991372fa0eeb317d2ddb7151
Dan Rufener <drufener@newrelic.com>
2018-12-29 01:46:39 -0800

    Add language model informed sorter

---
+++ src/main/java/steno/NGramFrequencySorter.java
@@ -0,0 +1,34 @@
+package steno;
+
+import edu.berkeley.nlp.lm.NgramLanguageModel;
+import edu.berkeley.nlp.lm.StupidBackoffLm;
+import javafx.util.Pair;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static edu.berkeley.nlp.lm.io.LmReaders.readGoogleLmBinary;
+
+public class NGramFrequencySorter {
+    private static final NgramLanguageModel<String> languageModel = loadLanguageModel();
+
+    private static NgramLanguageModel<String> loadLanguageModel() {
+        System.out.println("Loading Language Model (~1 minute)");
+        long startTime = System.currentTimeMillis();
+        //  Google Books binaries for English from http://tomato.banatao.berkeley.edu:8080/berkeleylm_binaries/
+        final StupidBackoffLm<String> lm = readGoogleLmBinary("/Users/drufener/steno/src/main/resources/eng.blm", "/Users/drufener/steno/src/main/resources/vocab_cs");
+        long endTime = System.currentTimeMillis();
+        System.out.println("Loaded Language Model Time (ms): " + (endTime - startTime));
+        return lm;
+    }
+
+    public static List<String> sortByLikelihoodDescending(final List<String> context, List<String> candidates) {
+        return candidates.stream().map(candidate -> {
+            final List<String> candidateNgram = new ArrayList<>(context);
+            candidateNgram.add(candidate);
+            return new Pair<>(candidate, languageModel.getLogProb(candidateNgram));
+        }).sorted(Comparator.comparingDouble((Pair p) -> (double) (float) p.getValue()).reversed())
+                .map((Pair p) -> (String) p.getKey())
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/steno/NGramFrequencySorterTest.java
@@ -0,0 +1,25 @@
+package steno;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+public class NGramFrequencySorterTest {
+
+    @Test
+    public void sortByLikelihoodDescending() {
+        ImmutableList<String> context = ImmutableList.of("I", "went");
+        ImmutableList<String> candidateNextWords = ImmutableList.of("of", "the", "to", "store", "over");
+        List<String> results = NGramFrequencySorter.sortByLikelihoodDescending(context, candidateNextWords);
+        assertThat(results).containsExactly("to", "over", "the", "of", "store");
+
+        context = ImmutableList.of("cross", "my");
+        candidateNextWords = ImmutableList.of("the", "path", "heart");
+        results = NGramFrequencySorter.sortByLikelihoodDescending(context, candidateNextWords);
+        assertThat(results).containsExactly("path", "heart", "the");
+    }
+}
