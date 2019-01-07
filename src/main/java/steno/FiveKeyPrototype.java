d73f2cba3c4e9b08df040a18e3db1abc0004cc7b
Dan Rufener <drufener@newrelic.com>
2019-01-06 19:11:32 -0800

    rename for accuracy to 5

--- src/main/java/steno/FiveKeyPrototype.java
+++ src/main/java/steno/FiveKeyPrototype.java
@@ -21,39 +21,7 @@ import static steno.Schemes.GENERIC_CLASS.*;
   Cancel: Space Bar
  */
 
-
-/*
-     * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
-     *
-     * Redistribution and use in source and binary forms, with or without
-     * modification, are permitted provided that the following conditions
-     * are met:
-     *
-     *   - Redistributions of source code must retain the above copyright
-     *     notice, this list of conditions and the following disclaimer.
-     *
-     *   - Redistributions in binary form must reproduce the above copyright
-     *     notice, this list of conditions and the following disclaimer in the
-     *     documentation and/or other materials provided with the distribution.
-     *
-     *   - Neither the name of Oracle or the names of its
-     *     contributors may be used to endorse or promote products derived
-     *     from this software without specific prior written permission.
-     *
-     * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-     * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-     * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-     * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-     * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-     * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-     * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-     * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-     * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-     * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-     * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-     */
-
-public class ThreeKeyPrototype extends JFrame implements KeyListener, ActionListener {
+public class FiveKeyPrototype extends JFrame implements KeyListener, ActionListener {
     JTextArea displayArea;
     JTextField typingArea;
     static final String newline = System.getProperty("line.separator");
@@ -97,7 +65,7 @@ public class ThreeKeyPrototype extends JFrame implements KeyListener, ActionList
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        ThreeKeyPrototype frame = new ThreeKeyPrototype("ThreeKeyPrototype");
+        FiveKeyPrototype frame = new FiveKeyPrototype("FiveKeyPrototype");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Set up the content pane.
@@ -136,7 +104,7 @@ public class ThreeKeyPrototype extends JFrame implements KeyListener, ActionList
         getContentPane().add(button, BorderLayout.PAGE_END);
     }
 
-    public ThreeKeyPrototype(String name) {
+    public FiveKeyPrototype(String name) {
         super(name);
     }
 
